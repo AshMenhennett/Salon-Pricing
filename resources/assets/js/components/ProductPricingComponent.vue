8502fd92418020f0142f63c403f0414bd2ffe256
Ashley Menhennett <ashleymenhennett@gmail.com>
2016-11-28 19:05:18 +1030

    Added quantity functionality to ProductPricingComponent and adjusted pricing list design

--- resources/assets/js/components/ProductPricingComponent.vue
+++ resources/assets/js/components/ProductPricingComponent.vue
@@ -7,8 +7,12 @@
             <ul class="list-group">
                  <li v-for="product in products" class="list-group-item">
                     <strong>{{ product.brand }}</strong>- {{ product.name }} : ${{ product.price }}
-                    <a href="#" v-if="!product.priceAdded" @click.prevent="addPrice(product.id, product.price)" class="btn btn-success pull-right">ADD</a>
-                    <a href="#" v-else @click.prevent="subPrice(product.id, product.price)" class="btn btn-danger pull-right">REMOVE</a>
+                    <span id="right-side-price-controls">
+                        <input type="number" v-model="product.qty_selected" min="1">
+                        <a href="#" v-if="!product.price_added" @click.prevent="addPrice(product.id, product.price)" class="btn btn-success">ADD</a>
+                        <a href="#" v-else @click.prevent="subPrice(product.id, product.price)" class="btn btn-danger">REMOVE</a>
+                        <span><span class="glyphicon glyphicon-shopping-cart"></span> {{ product.qty_added }}</span>
+                    </span>
                 </li>
             </ul>
         </div>
@@ -19,7 +23,7 @@
         <footer v-if="products.length" class="total-price-footer">
             ${{ total.toFixed(2).replace('-', '') }} AUD
             <br />
-            <a href="#" @click.prevent="clear()" class="clear-price">Clear Price</a>
+            <a href="#" @click.prevent="clear()" class="clear-price">Reset Price</a>
         </footer>
     </div>
 </template>
@@ -38,8 +42,12 @@
                 return this.$http.get('/products/fetch').then((response) => {
                     this.products = response.body;
                     for (var i = 0; i < this.products.length; i++) {
-                        // add priceAdded prop to each product obj
-                        this.products[i]['priceAdded'] = false;
+                        // when a product's price has been added to total, we want to reflect that in the ui
+                        Vue.set(this.products[i], 'price_added', false);
+                        // keep track of qty input element of each product
+                        Vue.set(this.products[i], 'qty_selected', 1);
+                        // keep track of the actual qty of the product
+                        Vue.set(this.products[i], 'qty_added', 0);
                     }
                 });
             },
@@ -47,25 +55,90 @@
                 this.products.reverse();
             },
             addPrice (id, price) {
-                this.total += parseFloat(price);
-                this.changePriceAddedProp(id, true);
+                // get the qty input selected
+                var qty = this.getProductQtySelected(id);
+                // set the qty selected to the product obj qty_added prop, saving the inputs state,
+                // rather than altering the obj prop for every change in input
+                this.setProductQtyAdded(id, qty);
+                this.total += (parseFloat(price) * qty);
+                this.setPriceAddedProp(id, true);
             },
             subPrice (id, price) {
-                this.total -= parseFloat(price);
-                this.changePriceAddedProp(id, false);
+                // qty input by user
+                var qty_selected = this.getProductQtySelected(id);
+                if (qty_selected > this.getProductQtyAdded(id)) {
+                    // if the qty_selected input is greater than the qty_added prop of product,
+                    // which is set when user adds qty of product
+                    this.total -= (parseFloat(price) * this.getProductQtyAdded(id));
+                    this.setProductQtyAdded(id, 0);
+                    this.setPriceAddedProp(id, false);
+                } else if (qty_selected < this.getProductQtyAdded(id)) {
+                    // see initial conditional statement comments for insight
+                    this.total -= (parseFloat(price) * qty_selected);
+                    this.setProductQtyAdded(id, (this.getProductQtyAdded(id) - qty_selected));
+                    if (this.getProductQtyAdded(id) > 0) {
+                        // if there is still a qty of the product after removing the qty_selected amount
+                        this.setPriceAddedProp(id, true);
+                    } else {
+                        this.setPriceAddedProp(id, false);
+                    }
+                } else {
+                    this.total -= (parseFloat(price) * qty_selected);
+                    this.setProductQtyAdded(id, 0);
+                    this.setPriceAddedProp(id, false);
+                }
+
+                if (this.getProductQtyAdded(id) === 0) {
+                    // fall back, just in case
+                    this.setPriceAddedProp(id, false);
+                }
+
+                if (parseFloat(this.total) < 0.001) {
+                    // if the total is essentially zero, clear everthing, just to make sure everything is functioning as expected
+                    this.clear();
+                }
             },
-            changePriceAddedProp (id, val) {
+            setPriceAddedProp (id, val) {
                 for (var i = 0; i < this.products.length; i++) {
                     if (this.products[i].id === id) {
-                        this.products[i].priceAdded = val;
+                        this.products[i].price_added = val;
+                    }
+                }
+            },
+            getPriceAddedProp (id) {
+                for (var i = 0; i < this.products.length; i++) {
+                    if (this.products[i].id === id) {
+                        return this.products[i].price_added;
+                    }
+                }
+            },
+            getProductQtySelected (id) {
+                for (var i = 0; i < this.products.length; i++) {
+                    if (this.products[i].id === id) {
+                        return this.products[i].qty_selected;
+                    }
+                }
+            },
+            setProductQtyAdded (id, qty) {
+                for (var i = 0; i < this.products.length; i++) {
+                    if (this.products[i].id === id) {
+                       this.products[i].qty_added = qty;
+                    }
+                }
+            },
+            getProductQtyAdded (id) {
+                for (var i = 0; i < this.products.length; i++) {
+                    if (this.products[i].id === id) {
+                       return this.products[i].qty_added;
                     }
                 }
             },
             clear () {
                 this.total = 0;
                 for (var i = 0; i < this.products.length; i++) {
+                    this.products[i].qty_added = 0;
                     if (this.products[i].priceAdded === true) {
-                        this.products[i].priceAdded = false;
+                        this.products[i].price_added = false;
                     }
                 }
             }
