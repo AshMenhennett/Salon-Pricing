1e37ea039d97e9727b40335c5fe5d5363f689ba4
Ashley Menhennett <ashleymenhennett@gmail.com>
2016-11-29 16:19:18 +1030

    Adding legal views for deploy to Heroku.

---
+++ resources/views/legal/privacy.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h4>Privacy Policy</h4></div>
+
+                <div class="panel-body">
+                    <p>The information that the user provides us on account sign up or by using the application or software in any way will solely be used for allowing us to provide the user with the services that the application or software provides. Part of the user's information may be required to be submitted to third-party companies or services to provide these services.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/legal/terms.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h4>Terms of Service</h4></div>
+
+                <div class="panel-body">
+                    <h4>Definitions:</h4>
+                    <ul>
+                        <li>The user refers to the end user, you.</li>
+                        <li>The application or software refers to any implementation of computer code, including websites</li>
+                        <li>Storage solutions accounts or services may refer to log files stored within the application or software, files or data being stored within the application or software, an active database used to store data by the application or software or a service, such as Amazon's S3 cloud storage that may be used by the application or software to store data and files.</li>
+                    </ul>
+
+                    <p>The following terms are a legally binding contract between the user and the owners and administrators of the application or software.</p>
+
+                    <p>All owners and administrators of the application and software, as well as the owners and administrators of the connected or affiliated storage solutions accounts or services in use, hold no responsibility for any user submitted data or files of any kind via the application, software or related services.</p>
+                    <p>All user submitted data is the sole responsibility of the user whom submitted the data.</p>
+                    <p>The user agrees to take full sole responsibility for the submission or storage of any illegal or unethical data or files of any kind.</p>
+                    <p>If the user abuses or uses the application or software in any way that is illegal or unethical (definable by the application or software owners and administrators), the owners and administrators of the application or software reserve the right to store evidence of the suspect activities or data, in order to provide it to legal entities in any locale.</p>
+                    <p>The user's account may be terminated if the user is suspected of using the application or software in any illegal or unethical behaviour, or to store or submit any illegal data of any kind. This means, any associated data or information will also be permanently deleted, and the application or software owners and administrators hold no liability for the loss of the related data or any associated financial loss resulting from the user's loss of data or information.</p>
+                    <p>In the event that the application or software fails in any way (bugs, unexpected behaviour, loss of data, including manual or automated closing or termination of user accounts and associated data or files), the application owners and administrators hold no liability on any resulting loss of the user's data or information including assets of any kind.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
