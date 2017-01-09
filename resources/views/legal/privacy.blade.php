4ce4e648f65492c02fa09f4f6be79b73da9e0779
Ashley Menhennett <ashleymenhennett@gmail.com>
2017-01-09 18:28:39 +1030

    Updated legal views

--- resources/views/legal/privacy.blade.php
+++ resources/views/legal/privacy.blade.php
@@ -9,6 +9,7 @@
 
                 <div class="panel-body">
                     <p>The information that the user provides us on account sign up or by using the application or software in any way will solely be used for allowing us to provide the user with the services that the application or software provides. Part of the user's information may be required to be submitted to third-party companies or services to provide these services.</p>
+                    <p>These policies may change without prior notice. It is the user's resonsibility to regularly check this document.</p>
                 </div>
             </div>
         </div>
--- resources/views/legal/terms.blade.php
+++ resources/views/legal/terms.blade.php
@@ -22,8 +22,10 @@
                     <p>The user agrees to take full sole responsibility for the submission or storage of any illegal or unethical data or files of any kind.</p>
                     <p>If the user abuses or uses the application or software in any way that is illegal or unethical (definable by the application or software owners and administrators), the owners and administrators of the application or software reserve the right to store evidence of the suspect activities or data, in order to provide it to legal entities in any locale.</p>
                     <p>The user's account may be terminated if the user is suspected of using the application or software in any illegal or unethical behaviour, or to store or submit any illegal data of any kind. This means, any associated data or information will also be permanently deleted, and the application or software owners and administrators hold no liability for the loss of the related data or any associated financial loss resulting from the user's loss of data or information.</p>
+                    <p>The user is solely responsible for any incurred costs as a result of using the application or software and any other related services.</p>
                     <p>In the event that the application or software fails in any way (including bugs, unexpected behaviour, loss of data, including deletion of the user's account(s) and associated data or files), the application owners and administrators hold no liability towards any resulting damages or financial costs of any kind.</p>
                     <p>In the event that the application or software is no longer offered (including complete removal of the application or software, denial of access or rejection of the user), the application owners and administrators hold no liability towards any resulting damages or possible discrimination.</p>
+                    <p>These terms may change without prior notice. It is the user's resonsibility to regularly check this document.</p>
                 </div>
             </div>
         </div>
