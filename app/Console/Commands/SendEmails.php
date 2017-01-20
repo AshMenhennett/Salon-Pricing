0bd495fa85515fb06eb6af102ea41ea957f79d7b
Ashley Menhennett <ashleymenhennett@gmail.com>
2017-01-20 16:21:27 +1030

    Adding test schedule email sending files

---
+++ app/Console/Commands/SendEmails.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\TestEmail;
+use Illuminate\Console\Command;
+
+class SendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \Mail::to('ashleymenhennett@gmail.com')->send(new TestEmail());
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\ClearCompiledViews::class,
+
+        Commands\SendEmails::class,
     ];
 
     /**
